9844dede0d062340b315e8abebd6bb2e7af2fb5c
Boomdie1422 <146508758+Boomdie1422@users.noreply.github.com>
2023-10-30 22:24:07 -0700

    Create Lab_4_vec.cpp

---
+++ lab4/Lab_4_vec.cpp
@@ -0,0 +1,32 @@
+/***
+ * CSE 2010 Fall 2023
+ * Lab #{4}
+ * {dan nguyen} #{007736788}
+ * {10/30/23}
+ *
+ * {the usage of arrays and vectors as well as defining each one to their defining roles in similarities/differences in order to understand the allocatetion of memory and their pointers/ 
+ * ( just find the similar and difference of arrays and vector and using to help with data structures)}
+ *
+ * {Describe 1) what was most challenging and 2) how you solved/overcame the problem OR
+ * the directions of how you PLAN to solve it}
+ *
+***/
+
+#include <iostream>
+#include "ArrayVec.h"
+#include "DisplayVec.h"
+using namespace std;
+
+
+int main()
+{
+    // initialize vec
+
+    //initialize ArrayVec and DisplayVec
+
+
+    // populate with values
+
+    // display contents
+
+}
