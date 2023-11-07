f68f30944b6b733e60adf4bcb2469f14880c254b
Boomdie1422 <146508758+Boomdie1422@users.noreply.github.com>
2023-11-07 00:00:39 -0800

    Add files via upload

--- lab4/Lab_4_vec.cpp
+++ lab4/Lab_4_vec.cpp
@@ -7,8 +7,7 @@
  * {the usage of arrays and vectors as well as defining each one to their defining roles in similarities/differences in order to understand the allocatetion of memory and their pointers/ 
  * ( just find the similar and difference of arrays and vector and using to help with data structures)}
  *
- * {Describe 1) what was most challenging and 2) how you solved/overcame the problem OR
- * the directions of how you PLAN to solve it}
+ * {Describe 1) the most challenging part was doing the display vec.cpp i ran inmto problems with linking v and manage to over came this by wipping it from the start and replace arr with v.
  *
 ***/
 
@@ -20,13 +19,26 @@ using namespace std;
 
 int main()
 {
-    // initialize vec
+    
+    // initialize vector
+    //vector<int> v;
+    const int CAPACITY = 10;
+    vector <int> test_vec(CAPACITY);
 
-    //initialize ArrayVec and DisplayVec
-
+    //initialize ArrayVec and DisplayArr
+    DisplayVec disp = DisplayVec();
+    ArrayVec av = ArrayVec();
 
     // populate with values
+    int range = 15;
+    int num_elem = 10;
+    av.randGenVec(test_vec, num_elem, range);
 
     // display contents
-
+    int test_vector_size = test_vec.size();
+    cout << "Vector Elements\n";
+    disp.printVector(test_vec);
+    cout << "Array Memsize\n";
+    disp.printMetaVector(test_vec);
 }
+
